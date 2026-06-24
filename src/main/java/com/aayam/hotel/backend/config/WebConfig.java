aa457ba384866ada6390fc3aced73a23832e7801
Vishal Kumar <calvk3@gmail.com>
2026-06-24 17:07:08 +0530

    Update Web Config

--- src/main/java/com/aayam/hotel/backend/config/WebConfig.java
+++ src/main/java/com/aayam/hotel/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Your frontend URL
+                .allowedOrigins("https://www.aayamregent.in/") // Your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
